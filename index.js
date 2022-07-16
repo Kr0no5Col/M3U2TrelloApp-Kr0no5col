6c6623965ba7db92a2ea4ef79d851bb97a1bc968
Kr0no5Col <andresespitia67@gmail.com>
2022-07-16 15:43:12 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let botonguardarcambios = document.getElementById('botonguardarcambios')
+let tituloinput = document.getElementById('tituloinput')
+
+botonguardarcambios.addEventListener('click', ()=> {
+    tituloinput ¿
+}
+)
